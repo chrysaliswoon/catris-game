5ce7fd80168cd6ec56730f8501b91d0675a37973
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2021-12-22 11:44:01 +0000

    Update constants.js

--- JS Folders/constants.js
+++ JS Folders/constants.js
@@ -9,6 +9,13 @@ const ROW = 20; // Height of the board is 20 rows
 // Size of the Blocks
 const BLOCK_SIZE = 30; // 
 
+// Drawing the Canvas // 
+// The document object provides a programmable interface for the HTML document. 
+// The document has a tree-like structure and is often referred to as the DOM (Document Object Model).
+const canvas = document.getElementById('board'); // We can call the getElementById method to access elements from the DOM.
+const ctx = canvas.getContext('2d'); // The HTMLCanvasElement.getContext() method returns a drawing context on the canvas, or null if the context identifier is not supported, or the canvas has already been set to a different context mode.
+
+
 // Arrow Keys - Numbers are taken from the keycodes for left, right, up and down: https://keycode.info/
 const KEY = {
     LEFT: 37,
--- JS Folders/main.js
+++ JS Folders/main.js
@@ -1,15 +1,5 @@
 // This will initialise the game and execute the overall game logic for Tetris
 
-// Drawing the Canvas // 
-
-// The document object provides a programmable interface for the HTML document. 
-// The document has a tree-like structure and is often referred to as the DOM (Document Object Model).
-
-const canvas = document.getElementById('board'); // We can call the getElementById method to access elements from the DOM.
-
-// The HTMLCanvasElement.getContext() method returns a drawing context on the canvas, or null if the context identifier is not supported, or the canvas has already been set to a different context mode.
-
-const ctx = canvas.getContext('2d');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
